f78e3ef9a84cc99888fcefe4e240e3aec5a889ee
roga <roga@oath.com>
2018-03-09 11:38:53 +0800

    change config format.

--- run.groovy
+++ run.groovy
@@ -2,54 +2,72 @@
 import groovyx.net.http.RESTClient
 
 gymList = [
-        gym1 : [
+        [
                 id : '5dee2e8612c24ba29d64a4493103bef3.16',
                 name : '永欣里公園 柱狀雕塑',
                 latitude : '25.123856',
                 longitude : '121.526775'
         ],
-        gym2 : [
+        [
                 id : 'bdd53b4d3d284d1d8e8c7719acf9dca4.16',
                 name : 'Fuxing Xmax Park (福興聖誕公園)',
                 latitude : '25.111196',
                 longitude : '121.51328'
         ],
-        gym3 : [
-                id : '96e8922f5e3b4498a921e078525f2d13.11',
-                name : '中研公園 Academia Park',
-                latitude : '25.047187',
-                longitude : '121.613656'
+        [
+                id : 'ad411927d28749dc833d6c4849dd6878.16',
+                name : '獅子會時鐘',
+                latitude : '25.121386',
+                longitude : '121.499351'
         ],
-        gym4 : [
+        [
                 id : '495431e7be6f405cb3b4d0d0450f3cd3.16',
                 name : '中庸二號公園',
                 latitude : '25.13723',
                 longitude : '121.501283'
         ],
-        gym5 : [
+        [
+                id : '92f03256f39b4f0f903f46fbc30e290a.16',
+                name : '復公綠白涼亭 (復興公園)',
+                latitude : '25.139179',
+                longitude : '121.503144'
+        ],
+        [
                 id : '3359ee9e8a2a43b891e0c5ab967c9c08.16',
                 name : '七星涼亭',
                 latitude : '25.136098',
                 longitude : '121.501942'
         ],
-        gym6 : [
+        [
+                id : '72036af021be4964af3599292c663811.16',
+                name : '圓形拱門',
+                latitude : '25.131961',
+                longitude : '121.503578'
+        ],
+        [
+                id : '3c1b5ea677ee4e0d999d3cc1e9dcdc9c.16',
+                name : '北投公園博物館',
+                latitude : '25.136753',
+                longitude : '121.5071'
+        ],
+        [
                 id : 'ca3a4709199540b19333b4d502741de2.16',
                 name : '燈光噴水地板',
                 latitude : '25.058067',
                 longitude : '121.615534'
         ],
-        gym7 : [
+        [
                 id : 'a01ef38f2a824dee801a174e1a3177ef.16',
                 name : '三貿公園',
                 latitude : '25.057866',
                 longitude : '121.614258'
         ],
-        gym8 : [
-                id : '7d19a592983b425a9773e8ce08c59375.11',
-                name : '天人境界',
-                latitude : '25.057556',
-                longitude : '121.617152'
-        ]
+        [
+                id : '96e8922f5e3b4498a921e078525f2d13.11',
+                name : '中研公園 Academia Park',
+                latitude : '25.047187',
+                longitude : '121.613656'
+        ],
 ]
 
 println "Raid Battle Detector.."
@@ -63,7 +81,7 @@ while(true) {
 
     } else {
 
-        gymList.each { key, gym ->
+        gymList.each { gym ->
 
             data = refreshBattle(gym)
             // println(data) // debug
